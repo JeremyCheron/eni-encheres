7e78ef17119b78dcf13bac0b7a1bdeb2621e3106
FannyRoux <fannyrouxfr@gmail.com>
2024-01-10 15:16:13 +0100

    Fanny : login + lougout cookie

--- src/main/java/fr/eni/encheres/ihm/servlets/user/Logout.java
+++ src/main/java/fr/eni/encheres/ihm/servlets/user/Logout.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -21,7 +22,16 @@ public class Logout extends HttpServlet {
         if (session != null) {
         	session.invalidate();
         }
-        
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("rememberMeCookie".equals(cookie.getName())) {
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                    break;
+                }
+            }
+        }
         response.sendRedirect(request.getContextPath());
 		
 	}
